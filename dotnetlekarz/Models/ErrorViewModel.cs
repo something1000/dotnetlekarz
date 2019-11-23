634f54b55dd372f58807bcf862f5947bbe43fba1
KamilJab <37241549+KamilJab@users.noreply.github.com>
2019-11-23 15:16:27 +0100

    initial

---
+++ dotnetlekarz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetlekarz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
